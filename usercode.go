6452eda5dbc6621ade72378c1acc6a67a53daffa
viplifes <viplifes@gmail.com>
2022-10-28 19:16:48 +0300

    Update usercode.go

--- usercode.go
+++ usercode.go
@@ -2,13 +2,16 @@ package main
 
 import (
 	"context"
-
 	"github.com/corezoid/gitcall-go-runner/gitcall"
 )
 
 func usercode(_ context.Context, data map[string]interface{}) error {
 
 	data["hello"] = "Hello world 2"
+	
+	if data["key1"] == "val1 {
+		data["test1"] = 123
+	}
 
 	return nil
 }
