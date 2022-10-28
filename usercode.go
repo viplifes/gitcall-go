99afca4c9b7e594f1710fd40ca4df735355ee7d3
viplifes <viplifes@gmail.com>
2022-10-28 19:08:41 +0300

    Create usercode.go

---
+++ usercode.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"context"
+
+	"github.com/corezoid/gitcall-go-runner/gitcall"
+)
+
+func usercode(_ context.Context, data map[string]interface{}) error {
+
+	data["hello"] = "Hello world 1"
+
+	return nil
+}
+
+func main() {
+	gitcall.Handle(usercode)
+}
